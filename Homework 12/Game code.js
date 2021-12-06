053de172ddcf01f4af08c1d93fa07dd322b1ac1a
nathanielspilker <legospilker@icloud.com>
2021-12-06 15:59:56 -0700

    changes

--- Homework 12/Game code.js
+++ Homework 12/Game code.js
@@ -1,7 +1,7 @@
 var playerX = 100;
 var playerY = 100;
 
-var w = 87;
+var w = 87; 
 var s = 83;
 var a = 65;
 var d = 68;
@@ -18,111 +18,95 @@ var enemyYSpeeds = [];
 
 var mouseshapeX;
 var mouseshapeY;
-function setup() {
-    createcanvas(500, 600);
-
+function setup()
+    createCanvas(500, 600);
     for (var i = 0; i < 50; i++) {
         enemyXSpeeds[i] = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
         enemyYSpeeds[i] = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
         enemyXs[i] = getRandomNumber(500);
         enemyYs[i] = getRandomNumber(600);
         diameters[i] = getRandomNumber(30);
-    }
-
-    createcharacter(200, 350);
 }
 
-function draw() {
-    background(120, 45, 78);
+function draw()
+{
+    background(600,600,600);
     stroke(0);
     fill(0);
 
-    createborders(10);
+    createborders();
 
-    textSize(16);
-    text("EXIT", width - 50, height - 50)
+    createcharacter();
 
-    drawcharacter();
     charactermovement();
 
-    fill(13, 145, 14);
+    createtext();
 
-    for (var i = 0; i < enemyXs.length; i++) {
-        circle(enemyXs[i], enemyYs[i], diameters[i]);
-        enemyXSpeeds[i] = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
-        enemyYSpeeds[i] = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
+    createmouseshape();
 
-        enemyXs[i] += enemyXSpeeds[i];
-        enemyYs[i] += enemyYSpeeds[i];
+    displayyouwin();
+    
+}
 
-        if (enemyXs[i] > width) {
-            enemyXs[i] = 0;
-        }
-        if (enemyXs[i] < 0) {
-            enemyXs[i] = width;
-        }
-        if (enemyYs[i] > height) {
-            enemyYs[i] = 0;
-        }
-        if (enemyYs[i] < 0) {
-            enemyYs[i] = height;
-        }
-    }
-
-    if (playerX > width && playerY > width - 50) {
+function displayyouwin()
+{
+    if(playerX > width && playerY > width-50)
+    {
         fill(0);
         stroke(5);
-        textSize(26);
-        text("You Win!", width / 2 - 50, height / 2 - 50);
-    }
-
-    fill(120, 130, 140);
-    circle(mouseshapeX, mouseshapeY, 25);
-}
-
-function charactermovement() {
-
-    if (keyIsDown(w)) {
-        playerY -= 10;
-    }
-    if (keyIsDown(s)) {
-        playerY += 10;
-    }
-    if (keyIsDown(a)) {
-        playerX -= 10;
-        console.log("movement: " + playerX);
-    }
-    if (keyIsDown(d)) {
-        playerX += 10;
+        textSize(20);
+        text("You are a Winner!", width/2-50, height/2-50);
     }
 }
 
-function createcharacter(x, y) {
-    playerX = x;
-    playerY = y;
+function createmouseshape()
+{
+    fill(0,0,0);
+    circle(mouseshapeX, mouseshapeY, 50);
 }
 
-function drawcharacter() {
-    fill(23, 40, 123);
-    circle(playerX, playerY, 25);
+function createtext()
+{
+    textSize(10);
+    text("EXIT", width-50,height-50)
 }
 
-function createborders(thickness) {
-
-    rect(0, 0, width, thickness);
-
-    rect(0, 0, thickness, height);
-
-    rect(0, height - thickness, width, thickness);
-
-    rect(width - thickness, 0, thickness, height - 50);
+function createcharacter()
+{
+    fill(0,0,800);
+    circle(playerX,playerY,25);
 }
 
-function mouseclicked() {
+function charactermovement()
+{
+    if(keyIsDown(w))
+    {
+        playerY -= 5;   
+    }
+    if(keyIsDown(s))
+    {
+        playerY += 5;   
+    }
+    if(keyIsDown(a))
+    {
+        playerX -= 5;   
+    }
+    if(keyIsDown(d))
+    {
+        playerX += 5;   
+    }
+}
+
+function createborders()
+{
+    rect(0,0,width,10);
+    rect(0,0,10,height);
+    rect(0, height-10,width, 10);
+    rect(width-10,0,10,height-50);
+}
+
+function mouseClicked()
+{
     mouseshapeX = mouseX;
     mouseshapeY = mouseY;
-}
-
-function getRandomNumber(number) {
-    return Math.floor(Math.random() * number) + 10;
 }
