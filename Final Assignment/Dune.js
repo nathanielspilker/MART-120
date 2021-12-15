9b2925f7a57bfd29a2763f79ff203b4429d4107c
nathanielspilker <legospilker@icloud.com>
2021-12-15 13:13:44 -0700

    Assignment finished

---
+++ Final Assignment/Dune.js
@@ -0,0 +1,71 @@
+function setup()
+{
+    createCanvas(500, 600);
+}
+
+function draw()
+{
+    background(0,0,100);
+ 
+    // dunes
+    fill(255, 204, 0);
+    circle(450,600,175);
+    fill(255, 204, 0);
+    circle(250,600,175); 
+    fill(255, 204, 0);
+    circle(150,600,175);
+    fill(255, 204, 0);
+    circle(50,600,175); 
+    fill(255, 204, 0);
+    circle(350,600,175);
+    fill(255, 204, 0);
+    circle(50,650,175);
+    fill(255, 204, 0);
+    circle(250,650,175); 
+    fill(255, 204, 0);
+    circle(150,650,175); 
+    fill(255, 204, 0);
+    circle(350,650,175); 
+    fill(255, 204, 0);
+    circle(450,650,175);
+
+    // stars
+    fill(500, 500, 500);
+    circle(200,390,10);
+    fill(500, 500, 500);
+    circle(285,212,10); 
+    fill(500, 500, 500);
+    circle(450,40,10);
+    fill(500, 500, 500);
+    circle(123,350,10); 
+    fill(500, 500, 500);
+    circle(149,167,10);
+    fill(500, 500, 500);
+    circle(42,468,10);
+    fill(500, 500, 500);
+    circle(367,449,10); 
+    fill(500, 500, 500);
+    circle(492,392,10); 
+    fill(500, 500, 500);
+    circle(310,40,10); 
+    fill(500, 500, 500);
+    circle(472,173,10);
+
+    // moon
+    fill(150, 150, 150);
+    circle(60,54,60);
+    fill(100, 100, 100);
+    circle(55,64,30);
+    fill(100, 100, 100);
+    circle(70,44,20);
+    fill(100, 100, 100);
+    circle(43,37,10);
+
+    fill(255,204,0);
+    textSize(62);
+    text("Dune",170,270);
+
+    fill(255,204,0);
+    textSize(42);
+    text("By Frank Herbert",90,310);
+}
